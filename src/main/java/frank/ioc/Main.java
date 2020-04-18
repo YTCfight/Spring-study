6ee06b5ac4e3a07a0aa60a71f624222313d10f75
yangtongchun <m15169658896@163.com>
2020-04-18 22:49:18 +0800

    杨同春

--- src/main/java/frank/ioc/Main.java
+++ src/main/java/frank/ioc/Main.java
@@ -1,23 +1,30 @@
 package frank.ioc;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 public class Main {
     public static void main(String[] args) {
         // 加载 Spring 的配置文件，初始化上下文：生成 Bean 对象
-        ApplicationContext context = new
-                ClassPathXmlApplicationContext("applications.xml");
-        // 通过名字获取 Bean 对象
-        Object o = context.getBean("myName");
-        System.out.println(o.getClass());
+//        ApplicationContext context = new
+//                ClassPathXmlApplicationContext("applications.xml");
+//        // 通过名字获取 Bean 对象
+//        Object o = context.getBean("myName");
+//        System.out.println(o.getClass());
+//        System.out.println(o);
+
+        //使用 BeanFactory 方式创建实例对象
+        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("/Users/yangtongchun/spring-study/src/main/resources/applications.xml"));
+        Object o = beanFactory.getBean("duck2");
         System.out.println(o);
 
 
-        Object duck1 = context.getBean("duck1");
-        System.out.println(duck1.getClass());
-        System.out.println(duck1);
+//        Object duck1 = context.getBean("duck1");
+//        System.out.println(duck1.getClass());
+//        System.out.println(duck1);
 //
 //        Object duck2 = context.getBean("duck2");
 //        System.out.println(duck2.getClass());
